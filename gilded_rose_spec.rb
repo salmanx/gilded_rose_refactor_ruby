b290855f3e647299bd44a73b8c9c89c1764ef862
Salman Mahmud <salman.mahmud@cressoftconsultancy.com>
2025-03-13 22:53:39 +0900

    Added both rspec and minitest for existing conditions

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -2,10 +2,137 @@ require 'rspec'
 
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 
-describe GildedRose do
-  it "does not change the name" do
-    items = [Item.new("foo", 0, 0)]
-    GildedRose.new(items).update_quality()
-    expect(items[0].name).to eq "fixme"
+RSpec.describe GildedRose do
+  def update_quality_for_days(gilded_rose, days)
+    days.times { gilded_rose.update_quality }
   end
+
+  let(:gilded_rose) { GildedRose.new([item]) }
+
+  describe ('Regular item (name: +5 Dexterity Vest, sell_in: 10, quality: 20') do
+    let(:item) { Item.new("+5 Dexterity Vest", 10, 20) }
+
+    it 'After 1 day it should have sell_in 9, quality 19' do
+      update_quality_for_days(gilded_rose, 1)
+      expect(item).to have_attributes(sell_in: 9, quality: 19)
+    end
+
+    it 'After 5 days it should have sell_in 5, quality 15' do
+      update_quality_for_days(gilded_rose, 5)
+      expect(item).to have_attributes(sell_in: 5, quality: 15)
+    end
+
+    it 'After 10 days it should have sell_in 0, quality 10' do
+      update_quality_for_days(gilded_rose, 10)
+      expect(item).to have_attributes(sell_in: 0, quality: 10)
+    end
+
+    it 'After 11 days it should have sell_in -1, quality 8' do
+      update_quality_for_days(gilded_rose, 11)
+      expect(item).to have_attributes(sell_in: -1, quality: 8)
+    end
+
+    it 'After 15 days it should have sell_in -5, quality 0' do
+      update_quality_for_days(gilded_rose, 15)
+      expect(item).to have_attributes(sell_in: -5, quality: 0)
+    end
+  end
+
+  describe ('Aged brie (name: Aged Brie, sell_in: 2, quality: 0') do
+    let(:item) { Item.new("Aged Brie", 2, 0) }
+
+    it 'After 1 day it should have sell_in 1, quality 1' do
+      update_quality_for_days(gilded_rose, 1)
+      expect(item).to have_attributes(sell_in: 1, quality: 1)
+    end
+
+    it 'After 2 days it should have sell_in 0, quality 2' do
+      update_quality_for_days(gilded_rose, 2)
+      expect(item).to have_attributes(sell_in: 0, quality: 2)
+    end
+
+
+    it 'After 5 days it should have sell_in -3, quality 8' do
+      update_quality_for_days(gilded_rose, 5)
+      expect(item).to have_attributes(sell_in: -3, quality: 8)
+    end
+
+    it 'After 10 days it should have sell_in -8, quality 18' do
+      update_quality_for_days(gilded_rose, 10)
+      expect(item).to have_attributes(sell_in: -8, quality: 18)
+    end
+
+    it 'After 50 days it should have sell_in -48, quality 50' do
+      update_quality_for_days(gilded_rose, 50)
+      expect(item).to have_attributes(sell_in: -48, quality: 50)
+    end
+
+    it 'After 55 days it should have sell_in -53, quality 50' do
+      update_quality_for_days(gilded_rose, 53)
+      expect(item).to have_attributes(sell_in: -51, quality: 50)
+    end
+
+  end
+
+  describe ('Sulfuras (name: Sulfuras, Hand of Ragnaros, sell_in: 2, quality: 0') do
+    let(:item) { Item.new("Sulfuras, Hand of Ragnaros", 2, 0) }
+
+    it 'After 1 day it should have sell_in 2, quality 0' do
+      update_quality_for_days(gilded_rose, 1)
+      expect(item).to have_attributes(sell_in: 2, quality: 0)
+    end
+
+    it 'After 2 days it should have sell_in 2, quality 0' do
+      update_quality_for_days(gilded_rose, 2)
+      expect(item).to have_attributes(sell_in: 2, quality: 0)
+    end
+
+  end
+
+
+  describe ('Backstage passes (name: Backstage passes to a TAFKAL80ETC concert, sell_in: 20, quality: 15') do
+    let(:item) { Item.new("Backstage passes to a TAFKAL80ETC concert", 20, 15) }
+
+    it 'After 1 day it should have sell_in 19, quality 16' do
+      update_quality_for_days(gilded_rose, 1)
+      expect(item).to have_attributes(sell_in: 19, quality: 16)
+    end
+
+    it 'After 5 days it should have sell_in 15, quality 20' do
+      update_quality_for_days(gilded_rose, 5)
+      expect(item).to have_attributes(sell_in: 15, quality: 20)
+    end
+
+    it 'After 11 days it should have sell_in 9, quality 27' do
+      update_quality_for_days(gilded_rose, 11)
+      expect(item).to have_attributes(sell_in: 9, quality: 27)
+    end
+
+    it 'After 17 days it should have sell_in 3, quality 41' do
+      update_quality_for_days(gilded_rose, 17)
+      expect(item).to have_attributes(sell_in: 3, quality: 41)
+    end
+
+    it 'After 19 days it should have sell_in 1, quality 47' do
+      update_quality_for_days(gilded_rose, 19)
+      expect(item).to have_attributes(sell_in: 1, quality: 47)
+    end
+
+    it 'After 20 days it should have sell_in 0, quality 50' do
+      update_quality_for_days(gilded_rose, 20)
+      expect(item).to have_attributes(sell_in: 0, quality: 50)
+    end
+
+
+    it 'After 21 days it should have sell_in -1, quality 0' do
+      update_quality_for_days(gilded_rose, 21)
+      expect(item).to have_attributes(sell_in: -1, quality: 0)
+    end
+
+
+  end
+
+
+
 end
+
--- gilded_rose_tests.rb
+++ gilded_rose_tests.rb
@@ -1,10 +1,108 @@
 require 'minitest/autorun'
 require_relative 'gilded_rose'
 
-class TestGildedRose < Minitest::Test
-  def test_foo
-    items = [Item.new("foo", 0, 0)]
-    GildedRose.new(items).update_quality()
-    assert_equal "fixme", items[0].name
+class GildedRoseTest < Minitest::Test
+  def update_quality_for_days(gilded_rose, days)
+    days.times { gilded_rose.update_quality }
+  end
+
+  def test_regular_item
+    item = Item.new("+5 Dexterity Vest", 10, 20)
+    gilded_rose = GildedRose.new([item])
+
+    update_quality_for_days(gilded_rose, 1)
+    assert_equal 9, item.sell_in
+    assert_equal 19, item.quality
+
+    update_quality_for_days(gilded_rose, 4)
+    assert_equal 5, item.sell_in
+    assert_equal 15, item.quality
+
+    update_quality_for_days(gilded_rose, 5)
+    assert_equal 0, item.sell_in
+    assert_equal 10, item.quality
+
+    update_quality_for_days(gilded_rose, 1)
+    assert_equal(-1, item.sell_in)
+    assert_equal 8, item.quality
+
+    update_quality_for_days(gilded_rose, 4)
+    assert_equal(-5, item.sell_in)
+    assert_equal 0, item.quality
+  end
+
+  def test_aged_brie
+    item = Item.new("Aged Brie", 2, 0)
+    gilded_rose = GildedRose.new([item])
+
+    update_quality_for_days(gilded_rose, 1)
+    assert_equal 1, item.sell_in
+    assert_equal 1, item.quality
+
+    update_quality_for_days(gilded_rose, 1)
+    assert_equal 0, item.sell_in
+    assert_equal 2, item.quality
+
+    update_quality_for_days(gilded_rose, 3)
+    assert_equal(-3, item.sell_in)
+    assert_equal 8, item.quality
+
+    update_quality_for_days(gilded_rose, 5)
+    assert_equal(-8, item.sell_in)
+    assert_equal 18, item.quality
+
+    update_quality_for_days(gilded_rose, 40)
+    assert_equal(-48, item.sell_in)
+    assert_equal 50, item.quality
+
+    update_quality_for_days(gilded_rose, 5)
+    assert_equal(-53, item.sell_in)
+    assert_equal 50, item.quality
+  end
+
+  def test_sulfuras
+    item = Item.new("Sulfuras, Hand of Ragnaros", 2, 0)
+    gilded_rose = GildedRose.new([item])
+
+    update_quality_for_days(gilded_rose, 1)
+    assert_equal 2, item.sell_in
+    assert_equal 0, item.quality
+
+    update_quality_for_days(gilded_rose, 1)
+    assert_equal 2, item.sell_in
+    assert_equal 0, item.quality
+  end
+
+  def test_backstage_passes
+    item = Item.new("Backstage passes to a TAFKAL80ETC concert", 20, 15)
+    gilded_rose = GildedRose.new([item])
+
+    update_quality_for_days(gilded_rose, 1)
+    assert_equal 19, item.sell_in
+    assert_equal 16, item.quality
+
+    update_quality_for_days(gilded_rose, 4)
+    assert_equal 15, item.sell_in
+    assert_equal 20, item.quality
+
+    update_quality_for_days(gilded_rose, 6)
+    assert_equal 9, item.sell_in
+    assert_equal 27, item.quality
+
+    update_quality_for_days(gilded_rose, 6)
+    assert_equal 3, item.sell_in
+    assert_equal 41, item.quality
+
+    update_quality_for_days(gilded_rose, 2)
+    assert_equal 1, item.sell_in
+    assert_equal 47, item.quality
+
+    update_quality_for_days(gilded_rose, 1)
+    assert_equal 0, item.sell_in
+    assert_equal 50, item.quality
+
+    update_quality_for_days(gilded_rose, 1)
+    assert_equal(-1, item.sell_in)
+    assert_equal 0, item.quality
   end
 end
